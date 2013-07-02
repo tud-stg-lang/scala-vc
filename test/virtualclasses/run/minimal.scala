20e6c33f79fdcde9410ad02b798452baf290077c
Manuel Weiel <manuel.weiel@stud.tu-darmstadt.de>
2013-07-02 12:12:39 +0200

    some testing classes

--- test/virtualclasses/run/minimal.scala
+++ test/virtualclasses/run/minimal.scala
@@ -2,6 +2,7 @@ class X {
 
   class Y <: {
     val bla : Y = new Y()
+    val foo = "Bar"
   }
 
 }
@@ -11,6 +12,7 @@ abstract class A {
   type B <: VC_TRAIT_T$B
 
   trait VC_TRAIT_T$B { self: B =>
+    //
     val bla : B = VC_NEW_T$B()
   }
   
@@ -25,6 +27,15 @@ class VC_FINAL_T$A extends A {
   }
   
   def VC_NEW_T$B(): B = new VC_FIX_T$B()
+}
+
+object F {
+  def main(args: Array[String]) {
+      //val x = new VC_FINAL$X()
+      //val y = x.VC_NEW$Y()
+      
+      println("Something works!")
+    }
 }
 
 // vim: set ts=4 sw=4 et:
